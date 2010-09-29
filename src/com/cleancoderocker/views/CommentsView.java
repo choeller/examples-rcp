975df703675e8d71bba5bf09236bc22b258508a1
Philip Ackermann <scalarocker@googlemail.com>
2010-09-29 14:11:41 +0200

    comments view

---
+++ src/com/cleancoderocker/views/CommentsView.java
@@ -0,0 +1,20 @@
+package com.cleancoderocker.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class CommentsView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
